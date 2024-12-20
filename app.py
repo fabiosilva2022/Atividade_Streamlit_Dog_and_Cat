cde596664589a97e03b2cd844eba4e1cf0ed191c
FABIO SILVA <115853940+fabiosilva2022@users.noreply.github.com>
2024-12-20 11:42:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 # Configuração da página
 st.title("Classificação de Gatos e Cachorros")
 st.write("Faça upload de uma imagem para classificar como 'Gato' ou 'Cachorro'.")
-colab_link = "[Clique aqui para acessar o notebook de treinamento no Google Colab](https://colab.research.google.com/drive/1sTRYiSgJsqAh0FZdq6BUOkidos1vV0W7#scrollTo=PLFqnHvIJSaW)"
+colab_link = "[Clique aqui para acessar o notebook de treinamento no Google Colab](https://colab.research.google.com/drive/1sTRYiSgJsqAh0FZdq6BUOkidos1vV0W7?usp=sharing)"
 st.markdown(f"### Treinamento do Modelo: {colab_link}")
 
 # Título da aplicação
